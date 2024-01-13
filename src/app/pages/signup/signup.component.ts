80dbb18b1affcf4a3fdbf99f783610163cf82f12
nikusha123456 <nikab6264@gmail.com>
2024-01-13 15:47:01 +0400

    io

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  isLogin: boolean = true;
 
+  ngOnInit(): void {}
+
+  toggleSwitch1() {
+    this.isLogin = !this.isLogin;
+  }
+
+  toggleSwitch2() {
+    this.isLogin = this.isLogin;
+  }
 }
