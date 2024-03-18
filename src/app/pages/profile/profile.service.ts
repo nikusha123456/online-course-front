aadc2b285676e8561c6cde62d418419d4d01e00d
nikusha123456 <nikab6264@gmail.com>
2024-03-18 19:28:23 +0400

    1

--- src/app/pages/profile/profile.service.ts
+++ src/app/pages/profile/profile.service.ts
@@ -9,22 +9,20 @@ export class ProfileService {
   private accessToken: string;
 
   constructor(private http: HttpClient) {
-    // Retrieve access token from local storage during service initialization
-    this.accessToken = localStorage.getItem('accessToken') ?? ''; // Use default value if null
+    
+    this.accessToken = localStorage.getItem('accessToken') ?? ''; 
   }
 
   getProfile(): Observable<any> {
-    // Check if access token is available
     if (!this.accessToken) {
       throw new Error('Access token not found');
     }
 
-    // Set up the HTTP headers with the access token
     const headers = new HttpHeaders().set(
       'Authorization',
       'Bearer ' + this.accessToken
     );
-    // Make the HTTP GET request with the headers
+
     return this.http.get<any>('http://localhost:3000/users/profile', {
       headers,
     });
