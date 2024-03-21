96400d9ebc76abaf5e8a7c7f19551154c4548f9e
nikusha123456 <nikab6264@gmail.com>
2024-03-21 18:17:57 +0400

    burger menu done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { AuthService } from '../pages/signup/auth.service';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,17 +11,27 @@ export class HeaderComponent implements OnInit {
   isClicked: boolean = false;
   isLoggedIn!: boolean;
   authService = inject(AuthService);
+  isBurgerMenuOpen: boolean = false;
 
-  ngOnInit(): void {}
+  toggleBurgerMenu() {
+    this.isBurgerMenuOpen = !this.isBurgerMenuOpen;
+  }
 
   toggleMenu() {
     this.isClicked = !this.isClicked;
   }
 
-  constructor() {
+  constructor(private router: Router) {
     this.isLoggedIn = !!localStorage.getItem('accessToken');
   }
 
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.isBurgerMenuOpen = false;
+      }
+    });
+  }
   logOut() {
     this.authService.logOut();
   }
